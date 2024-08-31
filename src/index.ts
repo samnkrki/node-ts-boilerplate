b3c750c2461a53ac34b4ec8141362beb605706e2
samnkrki <samn_krki@live.com>
2024-08-31 23:49:56 +0545

    feat: enclosed express app in a function

--- src/index.ts
+++ src/index.ts
@@ -9,51 +9,54 @@ import { handleApiError } from './middlewares/error-handler.middleware'
 import db from './config/db'
 
 const app = express()
-const port = configuration.port
+function bootstrap(app: express.Application): void {
+  const port = configuration.port
 
-// register middlewares
-app.use(cors())
-app.use(helmet())
-app.use(express.json())
-app.use(requestLogger)
+  // register middlewares
+  app.use(cors())
+  app.use(helmet())
+  app.use(express.json())
+  app.use(requestLogger)
 
-// database connection
-async function connectDatabase() {
-  logger.info('Connecting to database')
-  try {
-    await db.raw('SELECT 1+1 AS result')
-    logger.info('Connected to database')
-  } catch (err) {
-    logger.error('Error connecting to database', err)
-    process.exit(1)
+  // database connection
+  async function connectDatabase() {
+    logger.info('Connecting to database')
+    try {
+      await db.raw('SELECT 1+1 AS result')
+      logger.info('Connected to database')
+    } catch (err) {
+      logger.error('Error connecting to database', err)
+      process.exit(1)
+    }
   }
-}
 
-// routes register
-app.use('/api', appRoutes).use(handleApiError)
-app.get('/', (_req, res) => {
-  res.send('Hello, TypeScript with Express!')
-})
-app.get('/health', (_req, res) => {
-  res.send('Server is up and running!')
-})
-
-// Handle graceful shutdown
-process.on('SIGINT', async () => {
-  await db.destroy()
-  process.exit(0)
-})
-
-process.on('SIGTERM', async () => {
-  await db.destroy()
-  process.exit(0)
-})
-
-// start the server
-connectDatabase().then(() => {
-  app.listen(port, () => {
-    logger.info(`Server is running on port ${port}`)
+  // routes register
+  app.use('/api', appRoutes).use(handleApiError)
+  app.get('/', (_req, res) => {
+    res.send('Hello, TypeScript with Express!')
   })
-})
+  app.get('/health', (_req, res) => {
+    res.send('Server is up and running!')
+  })
+
+  // Handle graceful shutdown
+  process.on('SIGINT', async () => {
+    await db.destroy()
+    process.exit(0)
+  })
+
+  process.on('SIGTERM', async () => {
+    await db.destroy()
+    process.exit(0)
+  })
+
+  // start the server
+  connectDatabase().then(() => {
+    app.listen(port, () => {
+      logger.info(`Server is running on port ${port}`)
+    })
+  })
+}
+bootstrap(app)
 
 export default app
