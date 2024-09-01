ceeb45ee42f89f231188898c43e257196201bc1f
samnkrki <samn_krki@live.com>
2024-09-01 08:32:47 +0545

    refactor: commitlint keys

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,10 +4,6 @@ module.exports = {
   rules: {
     //   TODO Add Scope Enum Here
     // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf', 'vercel'],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf']],
   },
 }
